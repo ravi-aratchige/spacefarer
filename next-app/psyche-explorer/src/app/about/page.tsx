1e9d79a6623a017f10a8af01230cf4489cc69c6b
ravi-aratchige <rmaratchige@gmail.com>
2023-07-14 13:06:43 +0530

    Minor changes

--- next-app/psyche-explorer/src/app/about/page.tsx
+++ next-app/psyche-explorer/src/app/about/page.tsx
@@ -4,9 +4,11 @@ import Footer from "@/components/Footer";
 
 export default function About() {
     return (
-        <main>
+        <main className="flex flex-col justify-between min-h-screen bg-[#0f041E] text-white">
             <Navbar />
-            <h1>Lorem Ipsum</h1>
+            <section className="flex flex-col align-center text-center">
+                <h1 className="text-6xl p-10">About Psyche Explorer</h1>
+            </section>
             <Footer />
         </main>
     );
