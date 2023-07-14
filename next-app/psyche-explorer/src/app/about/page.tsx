7f005f1cc7e0cf4b8f6386fdf3d5eaafad9f7991
ravi-aratchige <rmaratchige@gmail.com>
2023-07-14 13:00:32 +0530

    Added routing to "About" page

---
+++ next-app/psyche-explorer/src/app/about/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+export default function About() {
+    return (
+        <main>
+            <Navbar />
+            <h1>Lorem Ipsum</h1>
+            <Footer />
+        </main>
+    );
+}
--- next-app/psyche-explorer/src/components/Navbar.tsx
+++ next-app/psyche-explorer/src/components/Navbar.tsx
@@ -4,7 +4,9 @@ import Image from 'next/image'
 export default function Navbar() {
     return (
         <nav className='flex justify-between p-5'>
-            <div>Psyche Explorer</div>
+            <div>
+                <a href="#">Psyche Explorer</a>
+            </div>
             <div>
                 <a href="#" className='px-8'>About</a>
                 <a href="#">Github</a>
