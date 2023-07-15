ec826028a4ff4a0fbdcf55a830299faa549580b8
ravi-aratchige <rmaratchige@gmail.com>
2023-07-16 03:08:35 +0530

    Added form page and fixed routing

--- flask-app/app.py
+++ flask-app/app.py
@@ -3,8 +3,13 @@ from flask import Flask, render_template, url_for
 app = Flask('Psyche Explorer')
 
 @app.route('/')
+@app.route('/home')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
