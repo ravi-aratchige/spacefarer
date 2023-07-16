574eb1c6fd84d49a7f238e02832a101c9a3416a1
ravi-aratchige <rmaratchige@gmail.com>
2023-07-16 11:36:17 +0530

    Fixed question switching in form
    Now, users can move from question to question as soon as they click a radio button.

--- flask-app/app.py
+++ flask-app/app.py
@@ -20,26 +20,31 @@ answers = []
 
 @app.route('/form-test', methods=['GET', 'POST'])
 def form_test():
-    if request.method == 'POST':
+    current_question_index = int(request.form.get('question_index', 0))
+    
+    while current_question_index < len(questions) - 1:
+        if request.method == 'POST':
         # Retrieve the selected answer from the form
-        answer = request.form.get('answer')
-        
-        # Process the selected answer, store it, or perform any other necessary actions
-        answers.append(answer)
-        
-        # Determine the index of the current question
-        current_question_index = int(request.form.get('question_index', 0))
-        
-        if current_question_index < len(questions) - 1:
-            # Increment the question index to display the next question
-            next_question_index = current_question_index + 1
-            return render_template('form_v2.html', question=questions[next_question_index], question_index=next_question_index)
+            answer = request.form.get('answer')
+            
+            print(answer)
+            
+            # Process the selected answer, store it, or perform any other necessary actions
+            answers.append(answer)
+            
+            # Determine the index of the current question
+            current_question_index = int(request.form.get('question_index', 0))
+            
+            if current_question_index < len(questions) - 1:
+                # Increment the question index to display the next question
+                next_question_index = current_question_index + 1
+                return render_template('form_v2.html', question=questions[next_question_index], question_index=next_question_index)
+            else:
+                # Display a thank you message or redirect to a different page
+                return render_template('home.html')
         else:
-            # Display a thank you message or redirect to a different page
-            return render_template('home.html')
-    else:
-        # Display the first question when initially accessing the page
-        return render_template('form_v2.html', question=questions[0], question_index=0)
+            # Display the first question when initially accessing the page
+            return render_template('form_v2.html', question=questions[0], question_index=0)
 
 if __name__ == '__main__':
     app.run(debug=True)
