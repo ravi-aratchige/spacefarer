54a499538588eb3caa0067c1c1b06f0eaa9a1194
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2023-08-06 23:54:35 +0530

    Added descriptive comment to form-handling function

--- flask-app/app.py
+++ flask-app/app.py
@@ -24,17 +24,23 @@ def form_v1():
 
     if request.method == 'POST':
 
+        # initializing variables to store total value for each personality trait
+
         openness_total = 0
         conscientiousness_total = 0
         extraversion_total = 0
         agreeableness_total = 0
         neuroticism_total = 0
 
+        # list of questions in web-form (for iteration)
+
         form_questions = ['q1', 'q2', 'q3', 'q4',
                           'q5', 'q6', 'q7', 'q8',
                           'q9', 'q10', 'q11', 'q12',
                           'q13', 'q14', 'q15', 'q16',
                           'q17', 'q18', 'q19', 'q20']
+        
+        # iterating through web-form responses to get values for each personality trait
 
         for question in form_questions[0:4]:
             openness_total = openness_total + int(request.form.get(question))
@@ -51,8 +57,7 @@ def form_v1():
         for question in form_questions[16:]:
             neuroticism_total = neuroticism_total + int(request.form.get(question))
 
-        print(f'openness_total is {openness_total}')
-        print(f'openness_total data type is {type(openness_total)}')
+        # print statements to test total scores for each personality trait
 
         print(f'openness_total is {openness_total}')
         print(f'conscientiousness_total is {conscientiousness_total}')
@@ -60,17 +65,23 @@ def form_v1():
         print(f'agreeableness_total is {agreeableness_total}')
         print(f'neuroticism_total is {neuroticism_total}')
 
+        # packing total scores into a single list
+
         scores = [openness_total,
                   conscientiousness_total,
                   extraversion_total,
                   agreeableness_total,
                   neuroticism_total]
         
+        # calculating the final scores (to be input to the model for prediction)
+        
         final_scores = []
 
         for score in scores:
             score = int(round(score / 2))
             final_scores.append(score)
+
+        # final print statement to check whether everything runs smoothly
 
         print(f'The final scores are {final_scores}')
 
